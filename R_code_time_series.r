394bc2c5f2725cda951a2282a2fabbaffa7bdf0f
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2021-04-01 21:15:45 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,9 @@
+# Time series analysis
+# Greenland increase of temperature
+# Data and code from Emanuela Cosma
+
+library(raster)
+setwd("/Users/ilari/Desktop/lab/greenland")
+
+install.packages("rasterVis")
+library(rasterVis)
