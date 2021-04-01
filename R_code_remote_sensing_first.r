8d904f9cf27220e0bc510946f8f625cf0c37bf45
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2021-04-01 20:31:08 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -5,7 +5,125 @@ setwd("/Users/ilari/Desktop/lab/")
 # nella lezione precedente install.packages("raster")
 
 library(raster)
-p224r63_2011 <- brick("p224r63_2011_masked.grd")
+p224r63_2011<-brick("p224r63_2011_masked.grd")
 p224r63_2011
 
 plot(p224r63_2011)
+
+#colour change
+cl <- colorRampPalette(c("black","grey","light grey")) (100)
+plot(p224r63_2011, col=cl)
+
+cls <- colorRampPalette(c("yellow","light green","blue")) (200)
+plot(p224r63_2011, col=cls)
+
+#LEZIONE 3
+
+library(raster)
+setwd("/Users/ilari/Desktop/lab/")
+p224r63_2011<-brick("p224r63_2011_masked.grd")
+
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+# dev.off will clean the current graph
+dev.off()
+plot(p224r63_2011$B1_sre)
+
+cls <- colorRampPalette(c("orange","yellow","light green")) (200)
+plot(p224r63_2011$B1_sre, col=cls)
+
+# 1 riga e 2 colonne
+par(mfrow=c(1,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# 2 righe e una colonna
+par(mfrow=c(2,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+# par(mfcol=c(1,2) dichiaro prima il numero di colonne
+
+
+par(mfrow=c(4,1))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+par(mfrow=c(2,2))
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+par(mfrow=c(2,2))
+clb <- colorRampPalette(c("dark blue","blue","light blue")) (200)
+plot(p224r63_2011$B1_sre, col=clb)
+
+clg <- colorRampPalette(c("dark green","green","light green")) (200)
+plot(p224r63_2011$B2_sre, col=clg)
+
+clr <- colorRampPalette(c("dark red","red","pink")) (200)
+plot(p224r63_2011$B3_sre, col=clr)
+
+cln <- colorRampPalette(c("orange","yellow","white")) (200)
+plot(p224r63_2011$B4_sre, col=cln)
+
+
+# LEZIONE 4
+# Visualizing data by RGB plotting
+
+# Bande Landsat
+# B1: blu
+# B2: verde
+# B3: rosso
+# B4: infrarosso vicino
+# B5: infrarosso medio
+# B6: infrarosso termico
+# B7: infrarosso medio
+
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+
+#pdf
+pdf("il_mio_primo_pdf_con_R.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+dev.off()
+
+
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Hist")
+
+#par natural colours, false colours, false colours with histogram stretch
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Hist")
+
+install.packages("RStoolbox")
+library(RStoolbox)
+
+#LEZIONE 5
