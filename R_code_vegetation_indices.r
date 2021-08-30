06add6f2d0526fdb127e1b92fdba74195c755fc6
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2021-08-30 19:15:35 +0200

    Create R_code_vegetation_indices.r

---
+++ R_code_vegetation_indices.r
@@ -0,0 +1,12 @@
+#R_code_vegetation_indices.r
+
+library(raster)
+setwd("/Users/ilari/Desktop/lab")
+
+defor1 <- brick("defor1.jpg")
+defor2 <- brick("defor2.jpg")
+# b1 = NIR, b2 = red, b3 = green
+
+par(mfrow=c(2,1))
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
