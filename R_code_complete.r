8cad1392f5708ddbb6e80a70a4fe12af4e461f28
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2021-12-28 17:58:45 +0100

    Update R_code_complete.r

--- R_code_complete.r
+++ R_code_complete.r
@@ -14,28 +14,31 @@
 # 9 - R Code Land Cover
 # 10 - R Code Variability
 #-----------------------------------------------
+#_______________________________________________
 
 # 1 - Remote Sensing First Code
 
 # My first code in R for remote sensing!
 
-#setwd: "Set Working Directory" permette di localizzare la cartella in cui trovare i file.
+#Set della working directory: la cartella da cui estraggo i dati per il codice. Dipende dal sistema operativo, su windows es: setwd("C:/lab")
 #le virgolette ("...")sono utilizzate quando devo uscire da R. 
 #nel percorso e fra la funzione e la parentesi non vanno inseriti spazi. 
 
 setwd("/Users/ilari/Desktop/lab/")
 
-# dovremo usare la funzione brick("...")
+# ..dovremo usare la funzione brick("...")
 #la funzione brick è all'interno del pacchetto raster. quindi dobbiamo richiamare il pacchetto installato nella lezione precedente install.packages("raster")
-#essendo già installato è  all'interno di R,non vanno messe le ".
-#per richiamre i pacchetti serve la funzione library.
+#Installare i pacchetti: mi permette di utilizzare le funzioni nei codici. install.packages(“raster”), utilizzo le virgolette perché sto uscendo da R. 
+#per richiamre i pacchetti serve la funzione library. Senza virgolette perché sono già dentro R (li ho installati): library(raster). 
+#Le librerie vanno richiamate sempre all’inizio. 
 
 library(raster)
 
-#birck() è la funzione per caricare dati in R. Va a importare i dati raster che abbiamo a disposizione. 
-#si usa brick se i dati raster sono dati dalla composizione di più bande (1 per ogni sensore).
+#Importare i dati: funzione brick: brick(“nome immagine.estensione”), con virgolette perché esco ed entro da R. 
+#Se subito dopo scrivo il nome dell’immagine posso ottenere le varie info: numero di pixel, sistema di riferimento.
+#Si usa brick se i dati raster sono dati dalla composizione di più bande (1 per ogni sensore).
+#Con il simbolo <- associamo il file ad un nome.
 
-#con il simbolo <- associamo il file ad un nome.
 p224r63_2011<-brick("p224r63_2011_masked.grd")
 p224r63_2011
 
@@ -48,11 +51,17 @@ p224r63_2011
 #nella B4 le piante hanno valori molto alti perchè riflettono molto.
 
 #plot è la funzione che permette di visualizzare l'immagine. non servono le virgolette perchè l'oggetto è già dentro a R.
+#La funzione restituirà una scala di colore di defoult. 
 
 plot(p224r63_2011)
 
 #Colour Change
-#uso il comando ColorRampPalette per cambiare la scala di colori visualizzata in un grafico.
+#Per cambiare la scala posso scegliere una color diversa con la funzione colorRampPalette. 
+#Colori indicati come etichette tra virgolette. 
+#Per dire al software che si tratta di elementi dello stesso tipo devo racchiuderli in un oggetto, un vettore (array).
+#Li raggruppo in parentesi con davanti una c, saranno elementi uniti per rappresentare un argomento, il colore. 
+#L’argomento con il numero indica quanti livelli di ciascun colore voglio visualizzare. 
+#Associo sempre il tutto ad un oggetto (cl) così da facilitare l’utilizzo del codice, posso scrivere solo l’oggetto e non tutta la funzione. 
 
 cl <- colorRampPalette(c("black","grey","light grey")) (100)
 
@@ -66,6 +75,7 @@ plot(p224r63_2011, col=cl)
 #importante è mettere la virgola tra gli argomenti.
 
 #Colour Change 2
+
 cls <- colorRampPalette(c("yellow","light green","blue")) (200)
 plot(p224r63_2011, col=cls)
 
@@ -96,8 +106,7 @@ plot(p224r63_2011$B1_sre)
 cls <- colorRampPalette(c("orange","yellow","light green")) (200)
 plot(p224r63_2011$B1_sre, col=cls)
 
-#la funzione par() permette un settaggio dei parametri grafici di un certo grafico che vogliamo creare.
-#nel nostro caso ci serve per fare un multiframe (mf) e baragonare due bande una affianco all'altra
+#Funzione PAR per plottare più immagini insieme nello stesso grafico e creare un multiframe. Mi permette di creare un sistema di righe e colonne. 
 
 # 1 riga e 2 colonne
 par(mfrow=c(1,2))
@@ -156,10 +165,10 @@ plot(p224r63_2011$B4_sre, col=cln)
 # B6: infrarosso termico
 # B7: infrarosso medio
 
-#schema RGB: colori di base Red, Green e Blue dai quali si ottengono tutti gli altri.
-#Possiamo usare le nostre bande Landsat per ottenere diverse visualizzazione nello schema RGB. 
-#Usiamo solo 1 banda per ogni componente. RGB=3 componenti, posso usare solo 3 bande per volta. 
-#Per ottenere una foto che rispecchia i colori visibili dall'occhio umano vanno rispettate le 3 componenti.
+#Per ogni singola lunghezza d’onda abbiam uno specifico sensore. Se uso i sensori del blu, rosso e verde vedrò come l’occhio umano, nel sistema RGB. 
+#Monto le tre bande una sull’altra. 
+#La banda del verde nella componente green del nostro sistema. E così per le altre due. Secondo un ordine 3-2-1 (rosso, verde, blu).
+#Argomento stretch: prendo i valori delle singole mappe e le “tiro” un po’ per fare in modo che non ci sia un schiacciamento verso una sola parte del colore. 
 #R=B3 (banda del rosso), G=B2 (banda del verde), B=B1 (banda del blu). 
 
 
@@ -167,7 +176,9 @@ plot(p224r63_2011$B4_sre, col=cln)
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 #l'elemento stretch(in questo caso lineare) prende la riflettanza delle singole bande e la "tira"  per evitare che ci sia una banda preponderante. 
 
+# Montiamo la 4 che è l’infrarosso sulla componente red, la 3 che è il rosso sulla componente green e la 2 che è il verde sulla componente blu. 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+
 #siccome abbiamo montato la B4 nella componente R di RGB e la vegetazione ha un'altissima riflettanza nel NIR,
 #la vegetazione prende il colore Red.
 #in generale per gli studi di vegetazione si usa l'IR sulla componente Red di RGB.
@@ -178,6 +189,8 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 #Il viola è la componente agricola, in questo caso.
 
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
+#Tutti gli oggetti che hanno alta riflettanza nella banda numero 4 assumeranno il colore della componente alla quale la banda numero 4 è stata associata.
+
 
 #uso par() per un riepilogo
 par(mfrow=c(2,2))
@@ -186,8 +199,11 @@ plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 
-#pdf 
+#Funzione pdf
 #per creare un pdf con i grafici appena creati va aggiunta la funzione pdf() prima della serie di funzioni.
+#Siamo dentro R, vogliamo prendere il grafico, uscire e portarlo nella cartella che ci interessa. 
+#Se usciamo o entriamo in R usiamo sempre le virgolette, con il nome dentro. 
+
 pdf("il_mio_primo_pdf_con_R.pdf")
 par(mfrow=c(2,2))
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
