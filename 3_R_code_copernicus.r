d3dde0279acedd55b7d59d3e1238f3c64918267b
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2022-03-03 16:42:56 +0100

    Update 3_R_code_copernicus.r

--- 3_R_code_copernicus.r
+++ 3_R_code_copernicus.r
@@ -8,13 +8,19 @@ library(raster)
 setwd("/Users/ilari/Desktop/lab")
 
 test <- raster("c_gls_SWI1km_202108181200_CEURO_SCATSAR_V1.0.1.nc")
+#la risoluzione è in gradi, non in metri, sono quindi coordinate geografiche.
+#il sistema di riferimento è il WGS84
 
 plot(test)
 clt <- colorRampPalette(c("dark green","green","white"))(100)
 plot(test, col=clt)
+
 #resampling: ricampionamento lineare, riduco i pixel
-testsam<- aggregate(test, fact=10)
+#questo dato si può ricampionare per ottenerne uno con pixel più grandi e alleggerirlo.
+#per ricampionare l'immagine uso la funzione aggregatetestsam<- aggregate(test, fact=10)
 plot(testsam, col=clt)
+#questo tipo di ricampionamento di chiama bilineare, prende 2 line di pixel e fa la media dei pixel all'interno.
+
 
 install.packages("knitr")
 install.packages("RStoolbox")
