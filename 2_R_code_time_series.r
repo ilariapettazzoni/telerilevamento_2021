5da36b98f93b6d84869b3c4448216b28f14989e9
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2022-03-03 16:40:31 +0100

    Update 2_R_code_time_series.r

--- 2_R_code_time_series.r
+++ 2_R_code_time_series.r
@@ -52,23 +52,31 @@ plot(TGr)
 
 #funzione RGB
 plotRGB(TGr, 1, 2, 3, stretch="Lin") 
+#con il plotRGB vediamo una sovrapposizione di 3 immagini, 2000-2005-2010. Siccome sulla componente green abbiamo messo il 2005.
+#se ho dei colori verdi ho dei valori più alti di lst nel 2005, se i colori sono rossi ho un lst più alto nel 2000, se ho colori blu ho dei valori più alti di lst nel 2010.
+
 plotRGB(TGr, 2, 3, 4, stretch="Lin") 
 plotRGB(TGr, 4, 3, 2, stretch="Lin") 
+
+#installo il pacchetto rasterVis (all'inizio)
 
 #LEZIONE 09/04/21
 
 #plottiamo i dati di copernicus assieme
 levelplot(TGr)
-#plot grafico prima immagine
+#plot grafico prima immagine, il dollaro lega lo strato dello stack allo stack stesso.
+
 levelplot(TGr$lst_2000)
+#con il plot di un singolo file ai lati dell'immagine ci sono dei grafici. questi grafici riportano il valore medio di lst della colonna o della riga.
+
 #cambio colore
-#col.regions per levelplot
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 
 #uso col.regions per levelplot
-#. Il levelplot è più potente di plot per output colori e mi permette di confrontare più facilmente, più compatto e potente.
+#Il levelplot è più potente di plot per output colori e mi permette di confrontare più facilmente, più compatto e potente.
 #Le coordinate sono solo sulla destra e sotto. I colori si vedono meglio. I titoli delle singole mappe sono i nomi dei vari livelli, possiamo anche cambiarli. 
 levelplot(TGr, col.regions=cl)
+#la differenza con il plot normale è che il levelplot ha una gamma di colori molto maggiore ed ha un outpot migliore.
 
 #rinomino gli attributi
 levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
@@ -78,12 +86,15 @@ levelplot(TGr,col.regions=cl, main="LST variation in time",
           names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
 
 # Melt
+#dati scioglimento
 meltlist <- list.files(pattern="melt")
 melt_import <- lapply(meltlist,raster)
 melt <- stack(melt_import)
 melt
 
 levelplot(melt)
+#useremo le immagini del '79 e 2007 per vedere la differenza nello scioglimento.
+#facciamo 2007-1979 più sarà alto il risultato maggiore sarà stato lo scioglimento
 #sottrazione tra l'ultimo e il primo dato per ottenere il livello di scioglimento, il $ lega lo stack allo strato interno 
 
 melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
