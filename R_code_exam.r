89efc3ed188dd00c82ec810be129a569645bbbf3
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2022-08-27 18:00:28 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -15,16 +15,77 @@ library(ggplot2)  # permette l'uso delle funzioni ggplot
 library(gridExtra)   # permette l'uso e creazione di griglie, tabelle e grafici
 library(rgdal) # per le firme spettrali
 library(grid) # Il pacchetto grid in R implementa le funzioni grafiche primitive che sono alla base del sistema di plottaggio ggplot2
+library (rasterdiv)
 # settaggio della working directory 
-setwd("/Users/ilari/Desktop/lab/")
+setwd("/Users/ilari/Desktop/lab/Esame/")
+
+Andes1 <- brick("andes_oli_20213321_lrg.jpg")
+Andes2 <- brick("andes_oli_20220151_lrg.jpg")
+
+par(mfrow=c(1,2))
+plotRGB(Andes1, r=1, g=2, b=3, stretch="lin")
+plotRGB(Andes2, r=1, g=2, b=3, stretch="lin")
+
+# par
+# visualizzo le 2 immagini assieme
+par(mfrow=c(1,2))
+plot(Andes1)
+plot(Andes2)
+
 # Importo i file tutti insieme (invece che singolarmente) utilizzando la funzione stack
 # Funzione list.files: crea lista di file per la funzione lapply 
+
 clist <- list.files(pattern="andes") # pattern = è la scritta in comune in ogni file, nel mio caso è columbia 
 # per ottenre le informazioni sui file 
 clist
-# [1] "1 columbia_tm5_1986209_lrg.jpg"          
-# [2] "2 columbia_tm5_1995202_lrg.jpg"          
-# [3] "3 columbia_etm_2001258_lrg.jpg"          
-# [4] "4 columbia_tm5_2005245_lrg.jpg"          
-# [5] "5 columbia_oli_2013203_lrg.jpg"          
-# [6] "6 columbiaglacier653_oli_2019172_lrg.jpg"
+# [1] "andes_oli_2021332_lrg.jpg" "andes_oli_2022015_lrg.jpg"
+
+# Funzione lapply: applica alla lista dei file una funzione (raster) 
+import <- lapply(clist,raster)
+# per ottenre le informazioni sui file
+import
+#[[1]]
+#class      : RasterLayer 
+#band       : 1  (of  3  bands)
+#dimensions : 3790, 3790, 14364100  (nrow, ncol, ncell)
+#resolution : 1, 1  (x, y)
+#extent     : 0, 3790, 0, 3790  (xmin, xmax, ymin, ymax)
+#crs        : NA 
+#source     : andes_oli_2021332_lrg.jpg 
+#names      : andes_oli_2021332_lrg 
+#values     : 0, 255  (min, max)
+
+
+#[[2]]
+#class      : RasterLayer 
+#band       : 1  (of  3  bands)
+#dimensions : 3790, 3790, 14364100  (nrow, ncol, ncell)
+#resolution : 1, 1  (x, y)
+#extent     : 0, 3790, 0, 3790  (xmin, xmax, ymin, ymax)
+#crs        : NA 
+#source     : andes_oli_2022015_lrg.jpg 
+#names      : andes_oli_2022015_lrg 
+#values     : 0, 255  (min, max)
+
+# Funzione stack: raggruppa e rinomina, in un unico pacchetto, i file raster separati
+ACi <- stack(import)
+# Funzione per avere le info sul file
+ACi
+
+#class      : RasterStack 
+#dimensions : 3790, 3790, 14364100, 2  (nrow, ncol, ncell, nlayers)
+#resolution : 1, 1  (x, y)
+#extent     : 0, 3790, 0, 3790  (xmin, xmax, ymin, ymax)
+#crs        : NA 
+#names      : andes_oli_2021332_lrg, andes_oli_2022015_lrg 
+#min values :                     0,                     0 
+#max values :                   255,                   255 
+
+
+# Funzione plot: del singolo file
+plot(ACi)
+# Funzione plotRGB: crea plot con immagini sovrapposte
+plotRGB(ACi, 3, 2, 1, stretch="hist")
+# Funzione ggr: plotta file raster in differenti scale di grigio, migliorando la qualità dell'immagine e aggiungengo le coordinate spaziali sugli assi x e y
+ggRGB(ACi, r=3, g=2, b=1, stretch="hist") # "hist": amplia i valori e aumenta i dettagli
+
