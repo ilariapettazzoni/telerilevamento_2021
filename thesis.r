b9fb41a83b3692885c066c953610aaabbfc8f3c1
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2022-10-31 13:04:23 +0100

    Update thesis.r

--- thesis.r
+++ thesis.r
@@ -1,5 +1,13 @@
 install.packages("GGally")
 library(GGally)
+library(raster)
+library(rgdal)
 
-ggpairs(iris[3:5], aes(color = Species, alpha = 0.5),
-        lower = list(combo = "count"))
+setwd("/Users/ilari/Desktop/")
+
+data2 <- read.table(file.choose("condambrid.csv"), header=T, sep=";")
+matrix <- ggpairs(data2)
+ggsave("corr.jpg", matrix, width = 15, height = 7) 
+ggsave("mtcars.pdf", )
+
+ggpairs(data2[3:5], aes(color = Altitudine, alpha = 0.5), lower = list(combo = "count"))
