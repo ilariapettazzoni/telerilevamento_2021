0962043f4e5c851d25c73beff72ba790e66308c0
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2022-11-27 23:32:03 +0100

    Update thesis.r

--- thesis.r
+++ thesis.r
@@ -11,3 +11,31 @@ ggsave("corr.jpg", matrix, width = 15, height = 7)
 ggsave("mtcars.pdf", )
 
 ggpairs(data2[3:5], aes(color = Altitudine, alpha = 0.5), lower = list(combo = "count"))
+
+
+setwd("/Users/ilari/Desktop/")
+
+my_data <- read.table(file.choose("ammcon.csv"), header=T, sep=";")
+set.seed(1234)
+dplyr::sample_n(my_data, 10)
+res.aov <- aov(amm ~ samp, data = my_data)
+# Summary of the analysis
+summary(res.aov)
+
+kruskal.test(weight ~ group, data = my_data)
+
+library(FSA)
+my_data <- read.table(file.choose("ammcon.csv"), header=T, sep=";")
+
+Dunn=dunnTest(pain ~ drug,
+         data=my_data,
+         method="bonferroni")
+library(multcompView)
+Dunnx = Dunn$res
+Dunnx
+Dunnx$P.adj
+
+library(rcompanion)
+cldList(P.adj ~ Comparison,
+        data = Dunnx,  threshold = 0.05, Letters=letters,  
+                reversed = FALSE)
