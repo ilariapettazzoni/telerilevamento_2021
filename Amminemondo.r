7f75d941d89e309a3b08c578a91a7a87b8c6a20d
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2023-07-11 15:03:56 +0200

    Amminemondo.r

--- Amminemondo.r
+++ Amminemondo.r
@@ -111,10 +111,10 @@ k4 <- kmeans(df, centers = 4, nstart = 25)
 k5 <- kmeans(df, centers = 5, nstart = 25)
 
 # plots to compare
-p1 <- fviz_cluster(k2, geom = "point", data = df) + ggtitle("k = 2")
-p2 <- fviz_cluster(k3, geom = "point",  data = df) + ggtitle("k = 3")
-p3 <- fviz_cluster(k4, geom = "point",  data = df) + ggtitle("k = 4")
-p4 <- fviz_cluster(k5, geom = "point",  data = df) + ggtitle("k = 5")
+p1 <- fviz_cluster(k2, geom = "text", data = df) + ggtitle("k = 2")
+p2 <- fviz_cluster(k3, geom = "text",  data = df) + ggtitle("k = 3")
+p3 <- fviz_cluster(k4, geom = "text",  data = df) + ggtitle("k = 4")
+p4 <- fviz_cluster(k5, geom = "text",  data = df) + ggtitle("k = 5")
 
 library(gridExtra)
 grid.arrange(p1, p2, p3, p4, nrow = 2)
