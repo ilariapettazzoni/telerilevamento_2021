71d75554993b62b794261e998c6f281c18e22847
ilariapettazzoni <81708735+ilariapettazzoni@users.noreply.github.com>
2023-09-05 12:21:44 +0200

    variance_tests.r

---
+++ variance_tests.r
@@ -0,0 +1,79 @@
+# AMMINE LIBERE
+
+setwd("/Users/ilari/Desktop/")
+x <- read.csv("statlibere.csv", header = T, sep=";", stringsAsFactors=F)
+head(x)
+
+#  sample       put        spd       spm      sum
+#1    563 1.0854995 0.04115958 1.0854995 2.212159
+#2    563 1.1038182 0.05473532 1.1038182 2.262372
+#3    563 1.4820428 0.05558634 1.4820428 3.019672
+#4    563 1.4459553 0.05753529 1.4459553 2.949446
+#5    616 0.8826149 0.04777415 0.8826149 1.813004
+#6    616 0.8617215 0.04136577 0.8617215 1.764809
+ 
+
+#____________________________________________
+
+# NORMALITY TEST
+# p-value > 0.05, accettiamo l'ipotesi nulla: siamo di fronte a una distribuzione normale.
+#p-value < 0.05, rifiutiamo l'ipotesi nulla: siamo di fronte a una distribuzione NON normale.
+
+#Anderson-Darling test for normality
+library(nortest)
+ad.test(x$sum)
+#data:  x$sum
+#A = 1.7207, p-value = 0.0001838
+
+
+#Cramer-von Mises normality test 
+library(nortest)
+cvm.test(x$sum)
+#data:  x$sum
+#W = 0.24663, p-value = 0.001346
+
+
+#Lilliefors (Kolmogorov-Smirnov) test for normality
+library(nortest)
+lillie.test(x$sum)
+#data:  x$sum
+#D = 0.13645, p-value = 0.007255
+
+#Shapiro-Francia normality test
+library(nortest)
+sf.test(x$sum)
+#data:  x$sum
+#W = 0.92137, p-value = 0.001479
+
+#____________
+#Test di analisi di omogeneità della varianza PER CAMPIONI NORMALI
+# p-value > 0.05. Quindi accettiamo (o per meglio dire, non rifiutiamo) l'ipotesi nulla: le varianze sono omogenee.
+#p-value < 0.05. Quindi rifiutiamo  l'ipotesi nulla: le varianze NON sono omogenee.
+
+#Test di Bartlett
+library(stats)
+bartlett.test(sum ~ sample, data=x)
+#data:  sum by sample
+#Bartlett's K-squared = 42.162, df = 14, p-value = 0.0001165
+
+
+#Test di Fligner-Killeen
+library(stats)
+fligner.test(sum ~ sample, data=x)
+#data:  sum by sample
+#Fligner-Killeen:med chi-squared = 48.675, df = 14, p-value = 1.016e-05
+
+#____________
+#Test di Levene non parametrico per campioni NON NORMALI
+
+library(car)
+leveneTest(sum ~ sample, data = x)
+#Levene's Test for Homogeneity of Variance (center = median)
+#      Df F value    Pr(>F)    
+#group 14  26.829 < 2.2e-16 ***
+#      45                      
+#---
+#Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+> 
+
+  
